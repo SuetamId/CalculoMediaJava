a4e43c9818c2c85f23e857af291d1d0e891e2565
GNTECH LAB <GNTECH LAB@Mateus>
2022-11-17 20:33:48 -0300

    Criando projeto

---
+++ primeiroprogramajava/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author GNTECH LAB
+ *
+ */
+module primeiroprogramajava {
+	
+
+}
---
+++ primeiroprogramajava/src/primeiroprogramajava/PrimeiraClassaJava.java
@@ -0,0 +1,9 @@
+package primeiroprogramajava;
+
+public class PrimeiraClassaJava {
+
+	/* Main é um metodo auto executavel em Java */
+	public static void main(String[] args) {
+		 
+		
+}
