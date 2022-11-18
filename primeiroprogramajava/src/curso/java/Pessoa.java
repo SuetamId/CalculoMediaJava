4520d70b09685abcd0e6c4a7da60893505f61a2b
GNTECH LAB <GNTECH LAB@Mateus>
2022-11-17 21:23:34 -0300

    Add new class Pessoa

---
+++ primeiroprogramajava/src/curso/java/Pessoa.java
@@ -0,0 +1,21 @@
+package curso.java;
+
+public class Pessoa{
+	
+	private String nome;
+	private Long id;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}
